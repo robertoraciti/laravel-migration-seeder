45f85112dd990358284ddb21e2130eb14e3f4588
robertoraciti <rob.raciti97@gmail.com>
2023-10-16 15:15:05 +0200

    add migration

---
+++ database/migrations/2023_10_16_124025_create_trains_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time')->nullable();
+            $table->time('arrival_time')->nullable();
+            $table->smallInteger('train_code');
+            $table->boolean('arriving');
+            $table->boolean('cancelled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
